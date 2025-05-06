4f47e25bc1d97008942ce39d86d36180e2b8cf09
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-05-05 18:59:31 -0600

    feat: add morgan middleware for logging HTTP requests in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   const configService = app.get(ConfigService);
   const port = configService.get<number>('config.api.port');
-  console.log(`API running on port ${port}`);
+  // Usar Morgan con formato predefinido
+  app.use(morgan('dev')); // Formato conciso para desarrollo
   await app.listen(port ?? 3000);
+  console.log(`API running on port ${port}`);
 }
 bootstrap();
