1562167155e28f63214c2347ab2aa6dfbd88b9d6
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-05-06 17:09:55 -0600

    feat: add user role enum and implement validation in CreateUserDto; update users controller to use ParseIntPipe

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // permite convertir strings a enums y otros tipos
+      whitelist: true, //esto ignora el campo que no esta validado
+      forbidNonWhitelisted: true, //esto nos alerta que hay un dato que no esta en la validación
+    }),
+  );
   const configService = app.get(ConfigService);
   const port = configService.get<number>('config.api.port');
   // Usar Morgan con formato predefinido
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+import { Role } from '@/common/enums/role.enum';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly fisrtName: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+  @IsEnum(Role, { message: 'Role must be either admin or basic' })
+  readonly role: Role;
+}
